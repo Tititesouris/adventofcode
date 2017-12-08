c93eb274ad660db0ee6901daf76ea139d4ae3c14
Tititesouris <tititesouris@laposte.net>
2017-12-08 20:46:02 +0100

    day 8 part 2

---
+++ day-8-2.py
@@ -0,0 +1,30 @@
+instructions = []
+while True:
+    line = input()
+    if line == "":
+        break
+    parts = line.split()[:]
+    register = parts[0]
+    cmd = parts[1]
+    val = int(parts[2])
+    condition = parts[4:]
+    instructions.append([register, cmd, val, condition])
+
+registers = {}
+maxVal = -999999999
+
+for instruction in instructions:
+    if instruction[0] not in registers:
+        registers[instruction[0]] = 0
+    if instruction[3][0] not in registers:
+        registers[instruction[3][0]] = 0
+    condition = str(registers[instruction[3][0]]) + " " + instruction[3][1] + " " + instruction[3][2]
+    if eval(condition):
+        if instruction[1] == "inc":
+            registers[instruction[0]] += instruction[2]
+        else:
+            registers[instruction[0]] -= instruction[2]
+    if registers[instruction[0]] > maxVal:
+        maxVal = registers[instruction[0]]
+
+print(maxVal)
