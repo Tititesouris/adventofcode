93318d2e4fe276c4b263fd48a999611c520d1ad8
Tititesouris <tititesouris@laposte.net>
2017-12-12 22:30:39 +0100

    day 12 part 1

---
+++ day-12-1.py
@@ -0,0 +1,24 @@
+instructions = {}
+while True:
+    line = input()
+    if line == "":
+        break
+    parts = line.split(" <-> ")
+    source = int(parts[0])
+    dests = list(map(int, parts[1].split(", ")))
+    instructions[source] = dests
+
+visited = []
+stack = [0]
+while len(stack) > 0:
+    programs = []
+    for program in stack:
+        visited.append(program)
+        for dest in instructions[program]:
+            if dest not in visited and dest not in stack:
+                programs.append(dest)
+    stack = programs[:]
+
+nbPrograms = len(visited)
+print(nbPrograms)
+
